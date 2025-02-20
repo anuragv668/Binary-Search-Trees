e7369cff133330cc0875643888eb5c1e98fcc0a9
anuragv668 <anuragvarma668@gmail.com>
2025-02-21 03:13:34 +0530

    add methods

--- bst.js
+++ bst.js
@@ -88,6 +88,103 @@ export default class Tree {
 
     return root;
   }
+
+  find(value, root = this.root ) {
+    if (!root) {
+      return null;
+    } 
+    if (root.data == value) {
+      return root;
+    }
+    if (root.data > value) {
+      return this.find(value, root.leftNode);
+    } 
+    if (root.data < value) {
+      return this.find(value, root.rightNode);
+    }
+  }
+
+  levelOrder(callback) {
+    let arr = [this.root];
+    while(arr[0]) {
+      callback(arr[0].data)
+      if (arr[0].leftNode) {
+        arr.push(arr[0].leftNode);
+      }
+      if (arr[0].rightNode) {
+        arr.push(arr[0].rightNode);
+      }
+      arr.shift()
+    }
+  }
+
+  inOrder(callback, root = this.root) {
+    if (root === null) {
+      return 
+    } 
+    this.inOrder(callback, root.leftNode);
+    callback(root.data);
+    this.inOrder(callback, root.rightNode);
+  }
+
+  preOrder(callback, root = this.root) {
+    if (root === null) {
+      return 
+    }
+    callback(root.data);
+    this.preOrder(callback, root.leftNode);
+    this.preOrder(callback, root.rightNode);
+  }
+
+  postOrder(callback, root = this.root) {
+    if (root === null) {
+      return 
+    } 
+    this.postOrder(callback, root.leftNode);
+    this.postOrder(callback, root.rightNode);
+    callback(root.data);
+  }
+  height(node = this.root) {
+    if (!node) {
+        return -1; // Convention: height of an empty node is -1
+    }
+
+    let leftHeight = this.height(node.leftNode);
+    let rightHeight = this.height(node.rightNode);
+
+    return 1 + Math.max(leftHeight, rightHeight);
+  }
+  
+  depth(node, root = this.root, level = 0) {
+    if (!root) {
+      return -1; 
+    }
+    if (root === node) {
+      return level; 
+    }
+
+    let leftDepth = this.depth(node, root.leftNode, level + 1);
+    let rightDepth = this.depth(node, root.rightNode, level + 1);
+
+    return Math.max(leftDepth, rightDepth); 
+  }
+
+  
+  isBalanced(root = this.root) {
+    let temp = this.height(root.leftNode) - this.height(root.rightNode);
+    if (temp == 0 || temp == 1 || temp == -1) {
+      return true;
+    }
+    return false;
+  }
+
+  reBalance() {
+  let arr = [];
+  this.inOrder((value) => arr.push(value));
+  this.root = this.buildTree(arr);
+  }
+
+//
 }
 
 export const prettyPrint = (node, prefix = "", isLeft = true) => {
